872427541c6e8b362afdafa79fa66ded5a3cdda6
include-minimaltools <gabrielortizamador@gmail.com>
2022-05-02 14:56:41 -0600

    Fragments menu

--- app/src/main/java/com/minimaltools/multiintentapp/view/MenuActivity.kt
+++ app/src/main/java/com/minimaltools/multiintentapp/view/MenuActivity.kt
@@ -2,11 +2,17 @@ package com.minimaltools.multiintentapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.minimaltools.multiintentapp.R
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val menu: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        setupWithNavController(menu, findNavController(this, R.id.fragment_menu))
     }
 }
