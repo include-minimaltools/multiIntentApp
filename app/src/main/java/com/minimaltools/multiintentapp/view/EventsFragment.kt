eb9675c65826a77bcb4b0931bb2e36e7377fcb9a
include-minimaltools <gabrielortizamador@gmail.com>
2022-05-02 12:57:36 -0600

    Initial commit

---
+++ app/src/main/java/com/minimaltools/multiintentapp/view/EventsFragment.kt
@@ -0,0 +1,17 @@
+package com.minimaltools.multiintentapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.minimaltools.multiintentapp.R
+
+class EventsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
---
+++ app/src/main/java/com/minimaltools/multiintentapp/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.minimaltools.multiintentapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.minimaltools.multiintentapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
